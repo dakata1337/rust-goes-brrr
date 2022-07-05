a081daa2dcc5716b1dbaa953ca39dd5b19777413
dakata1337 <poshtazaprezentaciaa@gmail.com>
2022-07-05 17:04:54 +0300

    Added .env file for configuration & Rust match counter

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,36 @@
+use dotenv::dotenv;
 use std::env;
+use std::sync::Arc;
 
 use serenity::async_trait;
 use serenity::model::channel::{Message, ReactionType};
 use serenity::model::gateway::Ready;
 use serenity::prelude::*;
 
-struct Handler;
+struct Handler {
+    pub rocket_counter: Arc<Mutex<usize>>,
+}
 
 #[async_trait]
 impl EventHandler for Handler {
     async fn message(&self, ctx: Context, msg: Message) {
-        if msg.content.to_lowercase().contains("rust") {
-            if let Err(why) = msg
-                .react(ctx.http, ReactionType::Unicode(String::from("🚀")))
-                .await
-            {
-                println!("Failed to react with `🚀`: {}", why);
+        let rust_counter = msg.content.to_lowercase().matches("rust").count();
+        if rust_counter == 0 {
+            return;
+        }
+
+        match msg
+            .react(ctx.http, ReactionType::Unicode(String::from("🚀")))
+            .await
+        {
+            Err(why) => println!("Failed to react with `🚀`: {}", why),
+            Ok(_) => {
+                let mut rocket_counter = self.rocket_counter.lock().await;
+                *rocket_counter += rust_counter;
+                println!(
+                    "{} has typed 'Rust' {} time(s). (Total: {})",
+                    msg.author.name, rust_counter, *rocket_counter
+                );
             }
         }
     }
@@ -27,6 +42,11 @@ impl EventHandler for Handler {
 
 #[tokio::main]
 async fn main() {
+    dotenv().ok();
+    let handler = Handler {
+        rocket_counter: Arc::new(Mutex::new(0)),
+    };
+
     let token = env::var("DISCORD_TOKEN")
         .expect("No DISCORD_TOKEN env variable provided");
     let intents = GatewayIntents::GUILD_MESSAGES
@@ -35,7 +55,7 @@ async fn main() {
         | GatewayIntents::GUILD_MESSAGE_REACTIONS;
 
     let mut client = Client::builder(&token, intents)
-        .event_handler(Handler)
+        .event_handler(handler)
         .await
         .expect("Err creating client");
 
